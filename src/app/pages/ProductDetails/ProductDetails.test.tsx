4dafbe7a4dc19a7dbd21f8aab85499c04b9ab7fa
Jaime9611 <jaimeescobar9611@gmail.com>
2023-01-28 18:04:55 -0500

    Product detail page added

---
+++ src/app/pages/ProductDetails/ProductDetails.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/lib/node';
+import { MemoryRouter } from 'react-router';
+import { Product } from '../../api/models/product';
+import { LOCAL_HOST } from '../../constants/paths';
+import Navigator from '../../routes/Navigator';
+import { setContext } from '../../tests/helpers';
+import { renderWithProviders } from '../../tests/test-utils';
+
+const responseJson = {
+  status: 'success',
+  message: 'Products Retrieved',
+  data: [
+    { id: 'ofjaifj2jr29fafjalfjla-jofj0q-fafjal', title: 'Call of Duty', price: 20.38 },
+    { id: 'ofjaifj2jr29fafjalfjla-jofj0q-fafackl', title: 'God of War 3', price: 34.38 },
+  ],
+};
+
+export const handlers = [
+  rest.get(`${LOCAL_HOST}/products`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(responseJson), ctx.delay(150));
+  }),
+];
+
+const server = setupServer(...handlers);
+
+beforeAll(() => server.listen());
+afterEach(() => server.restoreHandlers());
+afterAll(() => server.close());
+
+it('should render a Product Details page', async () => {
+  renderWithProviders(
+    <MemoryRouter initialEntries={[`/product/${responseJson.data[0].id}`]}>
+      <Navigator />
+    </MemoryRouter>,
+  );
+
+  expect(
+    await screen.findByRole('heading', { name: responseJson.data[0].title }),
+  ).toBeInTheDocument();
+});
---
+++ src/app/pages/ProductDetails/ProductDetails.tsx
@@ -0,0 +1,37 @@
+import { Container } from '@mui/system';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate, useParams } from 'react-router';
+import { fetchAllProducts } from '../../store/products/products.action';
+import { ProductState } from '../../store/products/productsSlice';
+import { AppDispatch } from '../../store/store';
+
+const ProductDetails = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const { id } = useParams<{ id: string }>();
+
+  const { products, loading, error } = useSelector(ProductState);
+  const productSelected = products.find(product => product.id === id);
+
+  useEffect(() => {
+    dispatch(fetchAllProducts());
+  }, []);
+
+  // TODO: Loading Component.
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <Navigate to='*' />;
+  }
+
+  return (
+    <Container>
+      <h1>{productSelected?.title}</h1>
+    </Container>
+  );
+};
+
+export default ProductDetails;
