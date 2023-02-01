bca26c8776c5a2e71adaeac7258838dde63e2d25
Jaime9611 <jaimeescobar9611@gmail.com>
2023-01-31 20:06:18 -0500

    Added adapters folder and productAdapters file. Ref #16

---
+++ src/app/adapters/productAdapters.test.ts
@@ -0,0 +1,7 @@
+import { productFromApi } from './productAdapters';
+
+describe('Product Model adapter', () => {
+  it('should have the function for adapt the product model', () => {
+    productFromApi();
+  });
+});
