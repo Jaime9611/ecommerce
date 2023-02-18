b9f5436928f39ad2d35bae3e061f11259a936547
Jaime9611 <jaimeescobar9611@gmail.com>
2023-02-18 17:52:35 -0500

    Solved test for dashboard

--- src/app/pages/Admin/components/Dashboard/Dashboard.test.tsx
+++ src/app/pages/Admin/components/Dashboard/Dashboard.test.tsx
@@ -1,8 +1,13 @@
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import Dashboard from './Dashboard';
 
 describe('Dashboard', () => {
   it('should have the Dashboard', () => {
-    render(<Dashboard />);
+    render(
+      <BrowserRouter>
+        <Dashboard />
+      </BrowserRouter>,
+    );
   });
 });
